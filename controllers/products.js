0cdfe1f17ada936a381c849d3a1f86b92727ab11
jerryspann <jerryspann0@gmail.com>
2025-04-12 18:38:45 -0700

    added the ability to add reviews for products and to edit the reviews

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,7 @@ const verifyToken = require('../middleware/verify-token.js');
 const Product = require('../models/product.js');
 const router = express.Router();
 
+// POST /products
 router.post("/", verifyToken, async (req, res) => {
     try {
         req.body.author = req.user._id;
@@ -14,6 +15,7 @@ router.post("/", verifyToken, async (req, res) => {
     }
 });
 
+// GET /products
 router.get("/", verifyToken, async (req, res) => {
     try {
         const products = await Product.find({})
@@ -25,15 +27,20 @@ router.get("/", verifyToken, async (req, res) => {
     }
 });
 
+// GET /products/:productId
 router.get("/:productId", verifyToken, async (req, res) => {
     try {
-        const product = await Product.findById(req.params.productId).populate("author");
+        const product = await Product.findById(req.params.productId).populate([
+            'author',
+            'reviews.author',
+        ]);
         res.status(200).json(product);
     } catch (err) {
         res.status(500).json({ err: err.message });
     }
 });
 
+// PUT /products/:productId
 router.put("/:productId", verifyToken, async (req, res) => {
     try {
         // Find the product:
@@ -61,6 +68,7 @@ router.put("/:productId", verifyToken, async (req, res) => {
     }
 });
 
+// DELETE /products/:productId
 router.delete("/:productId", verifyToken, async (req, res) => {
     try {
         const product = await Product.findById(req.params.productId);
@@ -71,6 +79,47 @@ router.delete("/:productId", verifyToken, async (req, res) => {
 
         const deletedProduct = await Product.findByIdAndDelete(req.params.productId);
         res.status(200).json(deletedProduct);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// POST /products/:productId/reviews
+router.post("/:productId/reviews", verifyToken, async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const product = await Product.findById(req.params.productId);
+        product.reviews.push(req.body);
+        await product.save();
+
+        // Find the newly created review:
+        const newReview = product.reviews[product.reviews.length - 1];
+
+        newReview._doc.author = req.user;
+
+        // Respond with the newReview:
+        res.status(201).json(newReview);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// PUT /products/:productId/reviews/:reviewId
+router.put("/:productId/reviews/:reviewId", verifyToken, async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.productId);
+        const review = product.reviews.id(req.params.reviewId);
+
+        // ensures the current user is the author of the review
+        if (review.author.toString() !== req.user._id) {
+            return res
+                .status(403)
+                .json({ message: "You are not authorized to edit this review" });
+        }
+
+        review.text = req.body.text;
+        await product.save();
+        res.status(200).json({ message: "Review updated successfully" });
     } catch (err) {
         res.status(500).json({ err: err.message });
     }
